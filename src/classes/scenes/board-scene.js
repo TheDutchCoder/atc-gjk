e1f0595d324dd73f87e34e5f41f3aec964caf335
Reinier Kaper <rp.kaper+github@gmail.com>
2022-04-22 17:17:13 -0400

    feat: :sparkles: Move the sun based on the time

--- src/classes/scenes/board-scene.js
+++ src/classes/scenes/board-scene.js
@@ -1,3 +1,5 @@
+import TWEEN from '@tweenjs/tween.js'
+
 import { gameService } from '#/state-machines/game'
 
 import GameScene from '#/classes/base/scene'
@@ -6,6 +8,7 @@ import GameBoard from '#/classes/base/game-board'
 import {
   HemisphereLight,
   DirectionalLight,
+  DirectionalLightHelper,
   Fog,
   Color,
   Object3D,
@@ -24,12 +27,17 @@ boardScene.start = () => {
   hemiLight.position.set(15, 25, 15)
 
   const pivot2 = new Object3D()
-  pivot2.rotation.x = Math.PI / -3
+  pivot2.name = 'sun'
+  // pivot2.rotation.x = Math.PI / -2
+  // pivot2.rotation.z = Math.PI / -2
   const dirLight1 = new DirectionalLight(0xffffff, 0.3)
+  const helper = new DirectionalLightHelper(dirLight1, 5)
   pivot2.add(dirLight1)
 
-  dirLight1.position.set(60, 0, 0)
-  dirLight1.lookAt(pivot2.position)
+  dirLight1.position.set(0, 60, 0)
+  // helper.target = pivot2
+  // dirLight1.lookAt(0 ,0 ,0)
+  // dirLight1.target = pivot2
   dirLight1.castShadow = true
   dirLight1.shadow.mapSize.width = 2048
   dirLight1.shadow.mapSize.height = 2048
@@ -40,9 +48,10 @@ boardScene.start = () => {
   dirLight1.shadow.camera.left = -90
   dirLight1.shadow.camera.right = 90
 
-  pivot2.rotation.y = Math.PI / -1.5
+  // pivot2.rotation.y = Math.PI / -1.5
 
   const dirLight2 = new DirectionalLight(0xb0e1ed, 0.2)
+  const helper2 = new DirectionalLightHelper(dirLight2, 5)
   dirLight2.position.set(-20, 25, -10)
 
   const pivot = new Object3D()
@@ -51,9 +60,14 @@ boardScene.start = () => {
 
   boardScene.addLight(hemiLight)
   boardScene.addLight(pivot2)
+  boardScene.addLight(helper)
+  boardScene.addLight(helper2)
   boardScene.addLight(dirLight2)
 
   const fog = new Fog(new Color(0x9bc8e9), 15, 250)
+
+  pivot2.rotation.x = Math.PI / 4 // How high the sun is in the sky
+  pivot2.rotation.z = Math.PI // The progress of the sun relative to time (0 is noon)
 
   board.generate()
 
@@ -86,8 +100,24 @@ boardScene.nextTick = async () => {
       return airplane.animateIn(0, 500)
     }))
 
+    // Rotate the sun
+    const sun = boardScene._lights.find(light => light.name === 'sun')
+
+    const moveSun =  new Promise((resolve) => {
+      const from = { rot: sun.rotation.z }
+      const to = { rot: sun.rotation.z + ((Math.PI * 2) * (1 / 96)) }
+
+      new TWEEN.Tween(from)
+      .to(to, 500)
+      .easing(TWEEN.Easing.Quadratic.InOut)
+      .onUpdate(() => sun.rotation.z = from.rot)
+      .onComplete(resolve)
+      .start()
+    })
+
     await moves
     await spawns
+    await moveSun
 
     boardScene.checkGhosts()
     // boardScene.checkCollisions()
