32cfd019c4edfdebeea3222536cb5ad0df2819f7
Reinier Kaper <rp.kaper+github@gmail.com>
2022-04-22 13:54:09 -0400

    fix: :bug: Remove stray loader

--- src/main.js
+++ src/main.js
@@ -3,8 +3,4 @@ import css from './index.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import { manager } from '#/loaders'
-
-manager.onLoad = () => {
-  createApp(App).mount('#app')
-}
+createApp(App).mount('#app')
